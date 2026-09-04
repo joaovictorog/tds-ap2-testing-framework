897a409b7e9c9e9cd0fdf1504db225af68d12d53
joaovictorog <jvog09@gmail.com>
2026-09-04 14:43:13 -0300

    4. Classe TestSuite

---
+++ 04_classe_testsuite/test_suite_test.py
@@ -0,0 +1,134 @@
+from testing_framework import TestCase, TestResult, TestSuite
+
+
+class TestStub(TestCase):
+    def test_success(self):
+        assert True
+
+    def test_failure(self):
+        assert False
+
+    def test_error(self):
+        raise Exception
+
+
+class TestSpy(TestCase):
+    def __init__(self, name):
+        TestCase.__init__(self, name)
+        self.was_run = False
+        self.was_set_up = False
+        self.was_tear_down = False
+        self.log = ""
+
+    def set_up(self):
+        self.was_set_up = True
+        self.log += "set_up "
+
+    def test_method(self):
+        self.was_run = True
+        self.log += "test_method "
+
+    def tear_down(self):
+        self.was_tear_down = True
+        self.log += "tear_down"
+
+
+class TestCaseTest(TestCase):
+    def set_up(self):
+        self.result = TestResult()
+
+    def test_result_success_run(self):
+        stub = TestStub("test_success")
+        stub.run(self.result)
+        assert self.result.summary() == "1 run, 0 failed, 0 error"
+
+    def test_result_failure_run(self):
+        stub = TestStub("test_failure")
+        stub.run(self.result)
+        assert self.result.summary() == "1 run, 1 failed, 0 error"
+
+    def test_result_error_run(self):
+        stub = TestStub("test_error")
+        stub.run(self.result)
+        assert self.result.summary() == "1 run, 0 failed, 1 error"
+
+    def test_result_multiple_run(self):
+        stub = TestStub("test_success")
+        stub.run(self.result)
+        stub = TestStub("test_failure")
+        stub.run(self.result)
+        stub = TestStub("test_error")
+        stub.run(self.result)
+        assert self.result.summary() == "3 run, 1 failed, 1 error"
+
+    def test_was_set_up(self):
+        spy = TestSpy("test_method")
+        spy.run(self.result)
+        assert spy.was_set_up
+
+    def test_was_run(self):
+        spy = TestSpy("test_method")
+        spy.run(self.result)
+        assert spy.was_run
+
+    def test_was_tear_down(self):
+        spy = TestSpy("test_method")
+        spy.run(self.result)
+        assert spy.was_tear_down
+
+    def test_template_method(self):
+        spy = TestSpy("test_method")
+        spy.run(self.result)
+        assert spy.log == "set_up test_method tear_down"
+
+
+class TestSuiteTest(TestCase):
+    def test_suite_size(self):
+        suite = TestSuite()
+
+        suite.add_test(TestStub("test_success"))
+        suite.add_test(TestStub("test_failure"))
+        suite.add_test(TestStub("test_error"))
+
+        assert len(suite.tests) == 3
+
+    def test_suite_success_run(self):
+        result = TestResult()
+        suite = TestSuite()
+        suite.add_test(TestStub("test_success"))
+
+        suite.run(result)
+
+        assert result.summary() == "1 run, 0 failed, 0 error"
+
+    def test_suite_multiple_run(self):
+        result = TestResult()
+        suite = TestSuite()
+        suite.add_test(TestStub("test_success"))
+        suite.add_test(TestStub("test_failure"))
+        suite.add_test(TestStub("test_error"))
+
+        suite.run(result)
+
+        assert result.summary() == "3 run, 1 failed, 1 error"
+
+
+if __name__ == "__main__":
+    result = TestResult()
+    suite = TestSuite()
+
+    suite.add_test(TestCaseTest("test_result_success_run"))
+    suite.add_test(TestCaseTest("test_result_failure_run"))
+    suite.add_test(TestCaseTest("test_result_error_run"))
+    suite.add_test(TestCaseTest("test_result_multiple_run"))
+    suite.add_test(TestCaseTest("test_was_set_up"))
+    suite.add_test(TestCaseTest("test_was_run"))
+    suite.add_test(TestCaseTest("test_was_tear_down"))
+    suite.add_test(TestCaseTest("test_template_method"))
+
+    suite.add_test(TestSuiteTest("test_suite_size"))
+    suite.add_test(TestSuiteTest("test_suite_success_run"))
+    suite.add_test(TestSuiteTest("test_suite_multiple_run"))
+
+    suite.run(result)
+    print(result.summary())
